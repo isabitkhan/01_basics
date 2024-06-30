8d3a43d76fd584126eb906b4ea48a0bf924ccba6
Sabit Khan <mesabitkhan@gmail.com>
2024-06-30 12:23:56 +0500

    basics datatypes

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123455;
+let accountEmail = "sabit@gmail.com";
+var accountPassword = "123Sabit";
+let accountState;
+
+console.table([accountEmail, accountId, accountId, accountState]);
