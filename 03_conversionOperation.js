076ee58462eb9c6ed36b3cf01fb7e508dccaec32
Sabit Khan <mesabitkhan@gmail.com>
2024-06-30 14:58:45 +0500

    comparission operators

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,11 +1,19 @@
 let score = "33";
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof score);
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let loggedIn = 1;
 let booleanLoggedIn = Boolean(loggedIn);
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+// ********** Comparission **********
+
+console.log("2" > 1); //true
+console.log("2" == 2); //true
+console.log("2" != 2); //false
+console.log("2" !== 2); //true
+console.log("2" === 2); //false
