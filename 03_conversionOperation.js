b2f740268e643e8108f435eaf9b19d93a834d8e1
Sabit Khan <mesabitkhan@gmail.com>
2024-06-30 13:15:38 +0500

    conversion of data types

---
+++ 03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33";
+
+console.log(typeof score);
+// console.log(typeof score);
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+let loggedIn = 1;
+let booleanLoggedIn = Boolean(loggedIn);
+console.log(booleanLoggedIn);
